6a8fc486071dc9da5a5496e6976987e4459a5239
Chen Fisher <chen@linux>
2013-11-01 16:26:07 +0200

    removed util

--- spec/util_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Btrack::Util do
-  context "granularity" do
-    it "should return expected format" do
-      Btrack::Util.granularity(:hourly, ).should eq ""
-    end
-  end
-end
