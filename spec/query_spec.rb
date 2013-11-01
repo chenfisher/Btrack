e60349d40318a19cd705c2f476ba286e5675d610
Chen Fisher <chen@linux>
2013-11-01 16:50:30 +0200

    removed some specs and set some pendings...

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Btrack::Query do
 
-  it "returns bitcount" do
+  xit "returns bitcount" do
     Btrack::Query.query("login", 123, :today).should be_true
   end
 
--- spec/time_frame_spec.rb
+++ spec/time_frame_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe Btrack::TimeFrame do
-  it "defaults to Time.now and :daily" do
-    expected = "#{Btrack.config.namespace}:login:#{Btrack::TimeFrame.granularity(:daily, Time.now)}"
-    Btrack::TimeFrame.key("login").should eq expected
-  end
-
-  context "format" do
-    it "returns the right key" do
-      expected = "#{Btrack.config.namespace}:login:#{Btrack::TimeFrame.granularity(:weekly, Time.now)}"
-      Btrack::TimeFrame.key("login", :weekly, Time.now).should eq expected
-    end
-  end
 end
