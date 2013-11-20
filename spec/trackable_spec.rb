ca31c681e2d004f616c3f952782157a09f816929
Chen Fisher <chen@linux>
2013-11-20 09:44:32 +0200

    added a spec to trackable

--- spec/trackable_spec.rb
+++ spec/trackable_spec.rb
@@ -7,7 +7,12 @@ class Dummy
   def dummy(user_id)
     puts user_id
   end
-  track :dummy, id: lambda{ |*args| args.first }, granularity: :daily, when: 3.days.ago, expiration_for:{daily: 1.minute}
+  track :dummy, key: "logged_in", id: lambda{ |*args| args.first }, granularity: :daily, when: 3.days.ago, expiration_for:{daily: 1.minute}
+
+  def no_key(user_id)
+    puts user_id
+  end
+  track :no_key, id: lambda{ |*args| args.first }, granularity: :daily, when: 3.days.ago, expiration_for:{daily: 1.minute}
 end
 
 describe Btrack::Trackable do
@@ -15,7 +20,12 @@ describe Btrack::Trackable do
 
   it "tracks a model's method" do
     dummy.dummy(999)
-    Btrack.redis.getbit(Btrack::Helper.key("dummy", :daily, 3.days.ago), 999).should eq 1
-    Btrack.redis.ttl(Btrack::Helper.key("dummy", :daily, 3.days.ago)).should eq 1.minute
+    Btrack.redis.getbit(Btrack::Helper.key("logged_in", :daily, 3.days.ago), 999).should eq 1
+    Btrack.redis.ttl(Btrack::Helper.key("logged_in", :daily, 3.days.ago)).should eq 1.minute
+  end
+
+  it "uses method name as the key" do
+    dummy.no_key(123)
+    Btrack.redis.getbit(Btrack::Helper.key("no_key", :daily, 3.days.ago), 123).should eq 1
   end
 end
