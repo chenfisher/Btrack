2ee6711e28d757a0f52ed502fb3ac18600589e90
Chen Fisher <chen.fisher@gmail.com>
2014-04-27 18:18:09 +0300

    count and exists - done

--- lib/btrack/query.rb
+++ lib/btrack/query.rb
@@ -13,16 +13,16 @@ module Btrack
     end
 
     def count
-      Btrack.redis.eval(lua_count, *@criteria.realize!)
+      Btrack.redis.eval(lua(:count), *@criteria.realize!)
     end
 
     def exists
       keys, args = @criteria.realize!
-      Btrack.redis.eval(lua_exists, *@criteria.realize!)
+      Btrack.redis.eval(lua(:exists), *@criteria.realize!)
     end
 
     private
-      def prefix
+      def lua(f)
         %Q{
           local index = 1
 
@@ -46,27 +46,20 @@ module Btrack
               redis.call('del', 'tmp:or')
             end
           end
+
+          #{send('lua_' + f.to_s)}
+
+          redis.call('del', 'tmp')
+          return results
         }          
       end
 
       def lua_count
-        %Q{
-          #{prefix}
-
-          local count = redis.call('bitcount', 'tmp')
-          redis.call('del', 'tmp')
-          return count
-        }
+        "local results = redis.call('bitcount', 'tmp')"
       end
 
       def lua_exists
-        %Q{
-          #{prefix}
-
-          local exists = redis.call('getbit', 'tmp', KEYS[#KEYS])
-          redis.call('del', 'tmp')
-          return exists
-        }
+        "local results = redis.call('getbit', 'tmp', KEYS[#KEYS])"
       end
   end
 end
