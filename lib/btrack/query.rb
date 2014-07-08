1c127b020674234a2cde6d6ec708c0466edde9fc
Chen Fisher <chen.fisher@gmail.com>
2014-07-08 10:14:05 +0300

    moved lua methods to different file; fixed query method missing

--- lib/btrack/query.rb
+++ lib/btrack/query.rb
@@ -1,4 +1,5 @@
 require 'btrack/query/criteria'
+require 'btrack/query/lua'
 
 module Btrack
   class Query
@@ -28,81 +29,8 @@ module Btrack
     class << self
       def method_missing(method, *args, &block)
         return unless method.to_s.end_with? '?'
-
-        Criteria.where(method.to_s.chomp('?') => args[1], id: args[0]).exists?
+        Criteria.where({method.to_s.chomp('?') => args[1]}, id: args[0]).exists?
       end
     end
-
-    private
-      def lua(f)
-        %Q{
-          local index = 1
-
-          for i, count in ipairs(ARGV) do
-            if count == '0' then
-              break
-            end
-
-            local bitop = {}
-
-            for c = index, index * count do
-              table.insert(bitop, KEYS[c])
-              index = index + 1
-            end
-
-            if i == 1 then
-              redis.call('bitop', 'or', 'tmp', unpack(bitop))
-            else
-              redis.call('bitop', 'or', 'tmp:or', unpack(bitop))
-              redis.call('bitop', 'and', 'tmp', 'tmp', 'tmp:or')
-              redis.call('del', 'tmp:or')
-            end
-          end
-
-          #{send('lua_' + f.to_s)}
-
-          redis.call('del', 'tmp')
-          return results
-        }          
-      end
-
-      def lua_count
-        "local results = redis.call('bitcount', 'tmp')"
-      end
-
-      def lua_exists
-        "local results = redis.call('getbit', 'tmp', KEYS[#KEYS])"
-      end
-
-      # lua script for plotting
-      # please note - it cannot be used with the prefixed 'lua' like count and exists
-      # this is a standalone script ment for plotting and allowing for cohort analysis
-      # all series must be of the same size
-      def plot_lua
-        %Q{
-          local series_count = #ARGV
-          local length = ARGV[1]
-
-          -- run over the first series
-          -- all series must be of the same size
-          local plot = {}
-          for i = 1, length do
-            local bitop = {}
-            for j = 1, series_count do
-              table.insert(bitop, KEYS[i*j])
-            end
-
-            -- make sure 'tmp' is populated with the first key (so the 'and' op would work as expected)
-            redis.call('bitop', 'or', 'tmp', 'tmp', bitop[1])
-
-            redis.call('bitop', 'and', 'tmp', unpack(bitop))
-            -- table.insert(plot, redis.call('bitcount', 'tmp'))
-            plot[KEYS[i]] = redis.call('bitcount', 'tmp')
-            redis.call('del', 'tmp')
-          end
-
-          return cjson.encode(plot)
-        }          
-      end
   end
 end
